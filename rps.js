032e5b2029dd329cfae40b3a045e850813df359f
coffeydoescode <seansince98@gmail.com>
2022-01-08 11:41:42 -0500

    MAdded additonal results and working on game function

--- rps.js
+++ rps.js
@@ -10,30 +10,58 @@ else if (choice === 1) result = "Paper";
 else result = "Scissors";
 // This code takes that number and assigns the variable result a value of rock, paper,
 // or scissors based on the random number
+let resultCleaned = result.toLowerCase();
+// console.log(resultCleaned);
+
 function computerPlay() {
-  return result;
+  return resultCleaned;
 }
 // This code defines the function computerPlay and returns the variable result, so that it returns rock, paper, or scissors
 const computerSelection = computerPlay();
-// This code places defines the variable computerSelection as the computerPlay function
-let playerSelection = "Rock";
+// This code defines the variable computerSelection as the computerPlay function
+
+let playerSelectionUnclean = window.prompt(
+  "Chooose Your Weapon! Rock, Paper, or Scissors"
+);
+let playerSelection = playerSelectionUnclean.toLowerCase();
 // This code takes a user input and and assigns the value to playerSelection (the actual prompt for user input still needs to be added)
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === "Rock" && computerSelection === "Scissors")
+  if (playerSelection === "rock" && computerSelection === "scissors")
     gameResult = "You Win!";
-  else if (playerSelection === "Rock" && computerSelection === "Paper")
+  else if (playerSelection === "rock" && computerSelection === "paper")
     gameResult = "You Lose!";
-  else if (playerSelection === "Rock" && computerSelection === "Rock")
+  else if (playerSelection === "rock" && computerSelection === "rock")
     gameResult = "You tied, that's lame!";
-  else gameResult = "You haven't defined that outcome!";
-  {
-    return gameResult;
-  }
+  else if (playerSelection === "paper" && computerSelection === "paper")
+    gameResult = "You tied, that's lame!";
+  else if (playerSelection === "paper" && computerSelection === "scissors")
+    gameResult = "You Lose!";
+  else if (playerSelection === "paper" && computerSelection === "rock")
+    gameResult = "You Win!";
+  else if (playerSelection === "scissors" && computerSelection === "paper")
+    gameResult = "You Win";
+  else if (playerSelection === "scissors" && computerSelection === "rock")
+    gameResult = "You Lose!";
+  else if (playerSelection === "scissors" && computerSelection === "scissors")
+    gameResult = "You tied, that's lame!";
+  else gameResult = "Check your spelling!";
+
+  return gameResult;
 }
 
-playRound(playerSelection, computerSelection);
+// playRound(playerSelection, computerSelection);
 // This code plays the round and declares the winner
+
+function game() {
+  playRound(playerSelection, computerSelection);
+  playRound(playerSelection, computerSelection);
+  playRound(playerSelection, computerSelection);
+  playRound(playerSelection, computerSelection);
+  playRound(playerSelection, computerSelection);
+}
+game();
+
 console.log(playerSelection);
 console.log(computerSelection);
 console.log(gameResult);
