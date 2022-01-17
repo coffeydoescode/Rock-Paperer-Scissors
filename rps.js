effb97c8e97fdd8a5f3d027533c7a8e084ec397b
coffeydoescode <seansince98@gmail.com>
2022-01-17 13:03:55 -0500

    Fixed Case Seneitivity

--- rps.js
+++ rps.js
@@ -1,54 +1,55 @@
-// let choice;
-// let result;
 function getRandomInt(max) {
   return (choice = Math.floor(Math.random() * max));
-}
-// getRandomInt(3);
-// console.log(getRandomInt(3));
-// console.log(choice);
-// This code returns a random number between 0 and 2
-// let choice = getRandomInt(3);
+} // This function returns a random number between 0 and 2
 
-// if (choice === 0) result = "rock";
-// else if (choice === 1) result = "paper";
-// else result = "scissors";
-// // This code takes that number and assigns the variable result a value of rock, paper,
-// // or scissors based on the random number
-
-// const computerSelection = computerPlay();
-// This code defines the variable computerSelection as the computerPlay function
-function assign(choice) {
+function assign() {
   if (choice === 0) result = "rock";
   else if (choice === 1) result = "paper";
   else result = "scissors";
   return result;
-}
-
-// assign(choice);
-// console.log(result);
+} // // This function takes that number and assigns the variable result a value of rock, paper,
+// // or scissors based on the random number
 
 function letComputerChoose() {
   getRandomInt(3);
   // console.log(choice);
   assign(choice);
   // console.log(result);
-  return result;
-}
+  computerSelection = result;
+} // This function gets a choice of rock, paper or scissors from the computer
+
+// TEST
+// letComputerChoose();
+// console.log(computerSelection);
 
 function letPlayerChoose() {
-  return (playerSelection = window.prompt(
-    "Chooose Your Weapon! Rock, Paper, or Scissors"
-  ));
-} // This variable prompts the user to input a choice of Rock, Paper, or Scissors
+  playerSelectionOld = window.prompt(
+    "Chooose Your Weapon! Rock, Paper, or Scissors",
+    "ROCK"
+  );
+  return playerSelectionOld;
+} // This function prompts the user to input a choice of rock, paper, or scissors
+
+function makeLower() {
+  return (playerSelection = playerSelectionOld.toLowerCase());
+} // This function takes the variable playerSelectionDirty and makes the string lower case
+
+function getPlayerChoice() {
+  letPlayerChoose();
+  makeLower();
+  return playerSelection;
+} // This fucntion takes user input and makes it lower case
 
 // letPlayerChoose();
+// console.log(playerSelectionOld);
+
+// makeLower();
 // console.log(playerSelection);
 
+// getPlayerChoice();
 // console.log(playerSelection);
-// let playerSelection = playerSelectionUnclean.toLowerCase();
-// This code takes a user input and and assigns the value to playerSelection (the actual prompt for user input still needs to be added)
 
-function playRound(playerSelection, computerSelection) {
+function playRound() {
   if (playerSelection === "rock" && computerSelection === "scissors")
     gameResult = "You Win!";
   else if (playerSelection === "rock" && computerSelection === "paper")
@@ -72,33 +73,50 @@ function playRound(playerSelection, computerSelection) {
   return gameResult;
 }
 
-// playRound(letPlayerChoose(), computerSelection);
+function testRound() {
+  if (playerSelection === "rock" && computerSelection === "rock")
+    result = "Cash Money! $$$";
+  else result = "FAIL";
+  return result;
+}
+// testRound(getPlayerChoice(), letComputerChoose());
+// console.log(result);
+// console.log(playerSelection);
+// console.log(computerSelection);
+
+// TEST
+// playRound(getPlayerChoice(), letComputerChoose());
+// console.log(playerSelection);
+// console.log(computerSelection);
+// console.log(gameResult);
 
 // This code plays the round and declares the winner
 
 function game() {
-  playRound(letPlayerChoose(), letComputerChoose());
+  playRound(getPlayerChoice(), letComputerChoose());
   console.log(playerSelection);
-  console.log(result);
+  console.log(computerSelection);
   console.log(gameResult);
-  playRound(letPlayerChoose(), letComputerChoose());
+  playRound(getPlayerChoice(), letComputerChoose());
   console.log(playerSelection);
-  console.log(result);
+  console.log(computerSelection);
   console.log(gameResult);
-  playRound(letPlayerChoose(), letComputerChoose());
+  playRound(getPlayerChoice(), letComputerChoose());
   console.log(playerSelection);
-  console.log(result);
+  console.log(computerSelection);
   console.log(gameResult);
-  playRound(letPlayerChoose(), letComputerChoose());
+  playRound(getPlayerChoice(), letComputerChoose());
   console.log(playerSelection);
-  console.log(result);
+  console.log(computerSelection);
   console.log(gameResult);
-  playRound(letPlayerChoose(), letComputerChoose());
+  playRound(getPlayerChoice(), letComputerChoose());
   console.log(playerSelection);
-  console.log(result);
+  console.log(computerSelection);
   console.log(gameResult);
 }
 game();
+// getPlayerChoice();
+// console.log(playerSelection);
 
 // console.log(playerSelection);
 // console.log(computerSelection);
